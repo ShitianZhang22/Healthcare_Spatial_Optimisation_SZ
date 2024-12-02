bd361cf77e67d7dce1b6008d5986d444cf4f6946
Shitian Zhang <2261700194@qq.com>
2024-12-02 21:15:08 +0000

    initial model

--- simulation/main.py
+++ simulation/main.py
@@ -11,6 +11,7 @@ class Patient(mesa.Agent):
     def __init__(self, model):
         super().__init__(model)
         self.stay = 1  # the remaining time a patient is forced at the current cell
+        self.start = model.steps  # the time when the patient entered the clinic
 
     def step(self):
         if self.stay == 0:
@@ -26,7 +27,8 @@ class Patient(mesa.Agent):
             self.stay = self.model.duration[new_pos]
             print('Patient {} is at {}.'.format(self.unique_id, self.pos[0]))
         else:
-            print('Patient {} has finished the test.'.format(self.unique_id))
+            duration = self.model.steps - self.start
+            print('Patient {} has finished the test. The total duration is {} mins.'.format(self.unique_id, duration))
             self.remove()
 
 
@@ -35,10 +37,6 @@ class Clinic(mesa.Model):
         super().__init__()
         self.grid = mesa.space.MultiGrid(7, 1, False)
         self.duration = [1, 5, 2, 16, 5, 0, 1]
-
-        # Create agents
-        for _ in range(1):
-            self.gen_patient()
 
     def gen_patient(self):
         """
@@ -49,12 +47,14 @@ class Clinic(mesa.Model):
 
     def step(self):
         """Advance the model by one step."""
-
+        print(self.steps)
+        if self.steps % 5 == 1:
+            self.gen_patient()
         self.agents.do('step')
 
 
 if __name__ == '__main__':
     clinic = Clinic()
-    for _ in range(100):
-        print(_)
+    for t in range(100):
+        # print(t)
         clinic.step()
